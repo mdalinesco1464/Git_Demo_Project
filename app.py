0776a46cfef0a1e2c1f6374cd93d5a07b150f4d4
mdalinesco1464 <mdalinesco1464@gmail.com>
2024-12-12 22:43:39 +0600

    Fix: update password secure alpha-numeric-special char combination/ create: hotfix-login branch swithc it

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ def login(username, password):
     """here use urs and pass to login"""
 
     username = "username"
-    password = "password"
+    password = "You#@234567S"
 
     return username, password
