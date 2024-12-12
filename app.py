9c788e4909b18be41f65caea5af2ac6ff86d9843
mdalinesco1464 <mdalinesco1464@gmail.com>
2024-12-12 22:29:10 +0600

    Fix: login bug in login function/ create: bugfix-login branchand swithc it

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-def login(username, password): pass
+"""simple login functinality """
+
+
+def login(username, password):
+    """here use urs and pass to login"""
+
+    username = "username"
+    password = "password"
+
+    return username, password
